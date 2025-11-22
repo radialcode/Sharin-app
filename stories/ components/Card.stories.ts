dc7c4030e507e8b4fa640c1865cc7cd6bae4fad9
Muskan saini <muskansaini758@gmail.com>
2025-11-22 14:54:10 +0530

    live link updated

--- stories/ components/Card.stories.ts
+++ stories/ components/Card.stories.ts
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import Card from "./Card";
 import { CARD_DEFAULTS } from "@/utils/helper";
 
-
 const meta = {
   title: "Components/Card",
   component: Card,
