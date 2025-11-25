183dd86bf9030997412931f6bda3370b60504232
Muskan saini <muskansaini758@gmail.com>
2025-11-25 15:17:27 +0530

    add var

--- stories/ components/Card.tsx
+++ stories/ components/Card.tsx
@@ -6,8 +6,10 @@ import React from "react";
 export interface CardData {
   title: string;
   imgSrc: string;
+  hoverImage: string;
   price: string;
   discount: string;
+  description?: string;
 }
 
 export interface CardProps {
@@ -18,17 +20,55 @@ export interface CardProps {
   titleClassName?: string;
   imgClassName?: string;
   priceClassName?: string;
-  colorMode?: 'light' | 'dark';
-  isSelected?: boolean;
+  color?: 'light' | 'dark';
+  added?: boolean;
+  favorite?: boolean;
+  description?: boolean;
+  overlay?: boolean;
+  screen?: 'desktop' | 'mobile'
+  radius?: 'default' | 'full'
+  status?: 'default' | 'hover'
+  badge?: boolean;
+  badgeSize?: 'LG 48' | 'MD 40' | 'SM 32'
+  badgeLeftIcon?: boolean
+  badgeRightIcon?: boolean
+  favoriteSize?: 'MD 40' | 'SM 32'
+  favoriteStatus?: 'hover' | 'focus' | 'loading' | 'disable' | 'default'
+  badgeType?: 'default' | 'primary' | 'success' | 'caution' | 'critical' | 'red' | 'yellow' | 'green' | 'sky' | 'purple' | 'inverce' | 'on color'
+  buttonSize?: 'LG 48' | 'MD 40' | 'SM 32'
+  buttonStatus?: 'hover' | 'focus' | 'loading' | 'disable' | 'default' | 'active'
+  buttonLeftIcon?: boolean
+  buttonRightIcon?: boolean
+  buttonText?: string
+   buttonAppearance?: "Subdued" | "Fill" | "Ghost" | "Critical" | "Accent" |"on color";
 
 }
 const Card: React.FC<CardProps> = ({
   card,
-  isSelected,
-  colorMode= TEXT_TYPE.LIGHT,
+  radius = 'defult',
+  buttonText = 'Aggiungi',
+  buttonSize = 'LG 48',
+  buttonAppearance,
+  buttonStatus = 'default',
+  buttonLeftIcon = false,
+  buttonRightIcon = false,
+  status = 'defult',
+  favoriteSize = 'MD 40',
+  favoriteStatus = 'default',
+  favorite = false,
+  description = false,
+  overlay = false,
+  screen = 'desktop',
+  badgeType = 'default',
+  badge = false,
+  badgeSize = 'SM 32',
+  badgeLeftIcon = false,
+  badgeRightIcon = false,
+  added = false,
+  color = TEXT_TYPE.LIGHT,
   className = "rounded-lg flex flex-col max-sm:justify-center max-lg:items-center group",
-  subCardClassName = "rounded-2xl border border-solid border-[#D9D9D9] max-w-[189px] size-[189px] max-sm:size-[unset] relative flex justify-center items-center bg-[#F1F1F1] group-hover:shadow-[0_1px_2px_rgba(0,0,0,0.15)] transition-shadow duration-300",
-  discountClassName = `absolute top-2 left-2 bg-white text-black flex items-center gap-1.5 text-sm font-semibold px-2 py-1 rounded-full  ${Number(
+  subCardClassName = "rounded-2xl  border border-solid border-[#D9D9D9] max-w-[189px] size-[189px] max-sm:size-[unset] relative flex justify-center items-center bg-[#F1F1F1] group-hover:shadow-[0_1px_2px_rgba(0,0,0,0.15)] transition-shadow duration-300",
+  discountClassName = `absolute top-2 px-2 py-1 left-2  flex items-center gap-1 font-semibold  rounded-full  ${Number(
     card.discount
       .replace("Guadagna €", "")
       .replace(/\./g, "")
@@ -39,14 +79,75 @@ const Card: React.FC<CardProps> = ({
     : ""
   }`,
   titleClassName = "text-lg font-black text-black mt-2 !mb-0 max-lg:text-center leading-[100%]",
-  imgClassName = "w-full mx-auto object-center h-auto object-contain rounded-2xl",
+  imgClassName = "w-full mx-auto object-center h-auto object-contain rounded-2xl group-hover:opacity-0 group-hover:pointer-events-none opacity-100 pointer-event-auto duration-300",
   priceClassName = "text-sm font-medium text-[#767676] mt-1",
+
 }) => {
+  const sizeBadge =
+    badgeSize === "MD 40"
+      ? " text-xs"
+      : badgeSize === "LG 48"
+        ? "text-sm"
+        : "text-[10px]";
+  const sizeButton =
+    buttonSize === "MD 40"
+      ? "text-xs"
+      : buttonSize === "LG 48"
+        ? "text-sm"
+        : "text-[10px]";
+  const sizeFavorite =
+    favoriteSize === "MD 40"
+      ? "size-10 min-w-10" : "size-8 min-w-8";
+       const appearanceButtonClasses =
+    buttonAppearance === "Fill"
+      ? "bg-black! text-white!"
+      : buttonAppearance === "Subdued"
+        ? "bg-white! text-black!"
+        : buttonAppearance === "Ghost" ?
+          "bg-white! text-black!" :
+          buttonAppearance === "Critical" ?
+            "text-white! bg-[#FC351B]!"
+            : buttonAppearance === "Accent" ?
+              "text-black! bg-[#FFE943]!"
+              : "bg-white! text-black!";
+  const badgeBgClasses = {
+    default: color === TEXT_TYPE.DARK ? "bg-[#191919] text-white" : "bg-gray-200 text-black",
+    primary: "bg-yellow-400 text-black",
+    success: "bg-green-600 text-black",
+    caution: "bg-yellow-400 text-black",
+    critical: "bg-red-500 text-white",
+    red: "bg-red-500 text-white",
+    yellow: "bg-yellow-400 text-black",
+    green: "bg-green-500 text-black",
+    sky: "bg-sky-400 text-black",
+    purple: "bg-purple-500 text-white",
+    inverce: "bg-black text-white",
+    "on color": "bg-white text-blackß",
+  };
+  const favoriteStatusClass = {
+    hover: "bg-[#191919] text-white",
+    focus: color === TEXT_TYPE.DARK ? "outline outline-white outline-offset-[3px]" : "outline outline-black outline-offset-[3px]",
+    loading: "",
+    disable: "opacity-50",
+    default: "bg-red-500 text-white",
+  };
+  const buttonStatusClass = {
+    hover: "bg-[#d4d4d4]! text-black",
+    focus: color === TEXT_TYPE.DARK ? "outline outline-white" : "outline outline-black",
+    loading: "",
+    disable: "group-hover:!opacity-50",
+    default: "bg-white text-black",
+    active: "bg-[#d4d4d4]! text-black",
+  };
+  const bgBadge = badgeBgClasses[badgeType] || badgeBgClasses.default;
+  const statusFav = favoriteStatusClass[favoriteStatus] || favoriteStatusClass.default;
+  const statusButton = buttonStatusClass[buttonStatus] || buttonStatusClass.default;
   return (
     <>
       <div className={className}>
-        <div className={`${subCardClassName} ${colorMode=== TEXT_TYPE.DARK?'bg-[#010101]!':''} ` }>
-          <span className={`${discountClassName} ${colorMode=== TEXT_TYPE.DARK?'text-[#f1f100]! bg-[#f1f1f1]/30! border-transparent!':''}`}>
+        <div className={`${subCardClassName} ${color === TEXT_TYPE.DARK ? 'bg-[#2b2b2b]!' : ''}  ${radius === 'full' ? 'rounded-full!' : ''}`}>
+          {badge && <span className={`${discountClassName} ${color === TEXT_TYPE.DARK ? '' : ''} ${sizeBadge} ${bgBadge}`}>
+            {badgeLeftIcon && <Icon name="circle" />}
             <span>
               {Number(
                 card.discount
@@ -72,14 +173,33 @@ const Card: React.FC<CardProps> = ({
                 )}
             </span>{" "}
             {card.discount}
-          </span>
+            {badgeRightIcon && <Icon name="eye" />}
+          </span>}
           <Image width={189} height={189} src={card.imgSrc} alt={card.title} className={imgClassName} />
-        { isSelected && <div className="bg-[#FFE943] absolute bottom-3 right-3 z-10 size-10 min-w-10 flex items-center justify-center rounded-2xl">
-            <Icon name="tick"/>
-          </div>}
+          <Image width={400} height={400} src={card.hoverImage} alt={card.title} className='group-hover:opacity-100 group-hover:pointer-events-auto opacity-0 pointer-event-none duration-300 z-[9] absolute inset-0 object-center  object-cover rounded-2xl' />
+
+          <div className="absolute bottom-2 left-0 left-0 z-10 flex gap-1 items-center right-0 mx-2">
+            <button className={`cursor-pointer relative w-full translate-y-5 opacity-0 group-hover:opacity-100 duration-300 left-0 right-0 mx-2 group-hover:translate-y-0 py-2 font-bold  rounded-2xl  hover:bg-white/80 ${sizeButton} ${statusButton} ${appearanceButtonClasses}`}> {buttonStatus === 'loading' ? <Icon className="text-black!" name="loading" /> : <span className="flex items-center justify-center gap-1"> {buttonLeftIcon && <Icon name="search"/>}
+                  {buttonText}
+                  {buttonRightIcon && <Icon className="rotate-90" name="moveRight"/>}</span>}</button>
+            {favorite && <div className={`bg-red-500  flex items-center justify-center rounded-lg ${sizeFavorite} ${statusFav}`}>
+              {favoriteStatus === 'hover' && <div className="bg-black py-2 px-2 !leading-[100%] text-white rounded-md absolute -top-[25px] text-[10px] text-nowrap font-semibold -transalte-y-1/2 flex flex-col items-center justify-center">
+
+                <p className="relative">Rimuovi dai preferiti
+                  <span className="w-1 h-1 min-w-1 bg-black rotate-45 absolute -bottom-3 left-1/2 -translate-1/2"></span>
+                </p>
+
+              </div>}
+              {favoriteStatus === 'loading' ? <Icon className="text-white" name="loading" /> : <Icon className="text-white" name="heart" />}
+            </div>}
+            {added && !favorite && <div className="bg-[#FFE943]  z-10 size-10 min-w-10 flex items-center justify-center rounded-2xl">
+              <Icon name="tick" />
+            </div>}
+          </div>
         </div>
-        <h3 className={`${titleClassName} ${colorMode=== TEXT_TYPE.DARK?'text-[#f1f100]!':''}`}>{card.title}</h3>
-        <p className={`${priceClassName} ${colorMode=== TEXT_TYPE.DARK?'text-[#f1f100]!':''}`}>{card.price}</p>
+        <h3 className={`${titleClassName} ${color === TEXT_TYPE.DARK ? 'text-white!' : ''} ${screen === 'mobile' ? 'text-sm!' : ''}`}>{card.title}</h3>
+        {description && <p className={`text-[#767676] text-sm mt-1 ${color === TEXT_TYPE.DARK ? 'text-white!' : ''}`}>{card.description}</p>}
+        <p className={`${priceClassName} ${color === TEXT_TYPE.DARK ? 'text-white!' : ''}`}>{card.price}</p>
       </div>
     </>
   );
